b9d853e14f7730f06585cce6f0bcf6478cb9f100
quangno129 <quangtran13121998@gmail.com>
2024-12-31 02:01:21 +0700

    test
    test

--- vars/createJobs.groovy
+++ vars/createJobs.groovy
@@ -1,12 +1,9 @@
 def call(String branchName = 'main') {
     node {
         stage("Checkout") {
-            withCredentials([string(credentialsId: 'github-token', variable: 'TOKEN')]) {
-                deleteDir()
-                sh """
-                  git clone https://github.com/quangno129/jenkins-dsl-v1 lib --depth 1
-                """
-            }
+            sh """
+              git clone https://github.com/quangno129/jenkins-dsl-v1 lib --depth 1
+            """
         }
 
         stage("Create folders and jobs") {
